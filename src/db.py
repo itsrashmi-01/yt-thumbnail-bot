c71d2e8ea5692e45f382e997a9c199cb98679d07
I'TS RASHMI <itsrashmi4@gmail.com>
2025-11-09 20:38:59 +0530

    Update db.py

--- src/db.py
+++ src/db.py
@@ -1,7 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 from .config import Config
+
 client = None
 db = None
+
 def init_db():
     global client, db
     if not Config.MONGO_URI:
@@ -9,16 +11,22 @@ def init_db():
     client = AsyncIOMotorClient(Config.MONGO_URI)
     db = client[Config.DB_NAME]
     return db
+
+
 async def ensure_user(collection, user):
+    """Add user to database if not already present."""
     if not collection:
         return
     await collection.update_one(
         {"id": user.id},
-        {"$setOnInsert": {
-            "id": user.id,
-            "username": user.username,
-            "first_name": user.first_name,
-            "added_on": __import__('datetime').datetime.utcnow()
-        }},
-        upsert=True
+        {
+            "$setOnInsert": {
+                "id": user.id,
+                "username": user.username,
+                "first_name": user.first_name,
+                "added_on": __import__("datetime").datetime.utcnow(),
+            }
+        },
+        upsert=True,
     )
+
