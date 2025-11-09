e9467fcad5fa5c0901880d960ab33ed9dc11d608
I'TS RASHMI <itsrashmi4@gmail.com>
2025-11-09 20:38:11 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,13 +1,15 @@
 from dotenv import load_dotenv
 import os
+
 load_dotenv()
+
 class Config:
     BOT_TOKEN = os.getenv("BOT_TOKEN")
     API_ID = int(os.getenv("API_ID") or 0)
     API_HASH = os.getenv("API_HASH")
     MONGO_URI = os.getenv("MONGO_URI")
     DB_NAME = os.getenv("DB_NAME", "ytthumbbot")
-    FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+    FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")  # @channel or -100...
     LOG_CHANNEL = int(os.getenv("LOG_CHANNEL") or 0)
     START_PIC = os.getenv("START_PIC")
     BOT_NAME = os.getenv("BOT_NAME", "YTThumbBot")
